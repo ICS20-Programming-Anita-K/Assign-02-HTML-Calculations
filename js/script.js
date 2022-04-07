486ad9a56734a1a6204e0d3c646315f8a1b82b61
Anita Kay <99274242+anitak456@users.noreply.github.com>
2022-04-07 21:18:45 +0000

    index.html

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// Created by: Anita Kay
+// Created on: April 2022
+// This file contains the JS functions for index.html
+
+"use strict"
+/**
+ * This function calculates volume of a octahedron.
+ */
+function calculate () {
+  // get the edge
+  let edge = parseInt(document.getElementById('edge').value)
+  // calculate the volume of the octahedron
+  const VOLUME = (Math.sqrt(2)/3)*(edge**3)
+  const AREA = 2*Math.sqrt(3)*(edge**2)
+  //display the volume of the octahedron
+  document.getElementById('answers').innerHTML = 'The volume is ' + VOLUME.toFixed(2) + 'cm<sup>3</sup>, and the surface area is ' + AREA.toFixed(2) + "cm<sup>2</sup>.";
+}
